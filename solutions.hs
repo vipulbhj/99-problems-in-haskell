ddd909481ece5a0784f03827e47ac061e822d61b
Vipulbhj <vipulb@geekyants.com>
2021-11-03 18:52:18 +0530

    Solutions from problem 14 to 19

--- solutions.hs
+++ solutions.hs
@@ -2,8 +2,8 @@ import Data.List
 
 data NestedList a = Elem a | List [NestedList a]
 
-data Quantify size char = Single char | Multiple size char
-                            deriving Show
+data Quantify a = Single a | Multiple Int a
+                    deriving (Show)
 
 -- Problem 01
 myLast []   = error "Are you dumb ..??"
@@ -56,12 +56,55 @@ encode :: Eq a => [a] -> [(Int, a)]
 encode = map (\x -> (length x, head x)) . Data.List.group
 
 -- Problem 11
-encodeModified :: Eq a => [a] -> [Quantify Int a]
+encodeModified :: Eq a => [a] -> [Quantify a]
 encodeModified = map (\x -> if (length x) > 1 then Multiple (length x) (head x) else Single (head x)) . Data.List.group
 
 -- Problem 12
-decodeModified :: [Quantify Int a] -> [a]
+decodeModified :: [Quantify a] -> [a]
 decodeModified = concatMap decodeModified'
                   where decodeModified' (Single a) = [a]
                         decodeModified' (Multiple times a) = replicate times a
+
+-- Problem 13
+-- Not sure what the problem means, skipping fro now.
+
+-- Problem 14
+dupli :: [a] -> [a]
+dupli = concatMap (replicate 2)
+
+
+-- Problem 15
+repli :: [a] -> Int -> [a]
+repli xs times = concatMap (replicate times) xs
+
+
+-- Problem 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs pos = loopWithIdx xs 1
+                    where loopWithIdx [] _ = []
+                          loopWithIdx (x:xs) idx
+                            | idx `mod` pos == 0 = loopWithIdx xs (idx + 1)
+                            | otherwise          =  x : loopWithIdx xs (idx + 1)
+
+-- Problem 17
+split :: [a] -> Int -> ([a], [a])
+split xs pos = ((take pos xs), (drop pos xs))
+
+-- Problem 18
+slice :: [a] -> Int -> Int -> [a]
+slice xs start end = take (end - start + 1) $ drop (start - 1) xs 
+
+-- Problem 19
+rotate :: [a] -> Int -> [a]
+rotate xs 0 = xs
+rotate [] _ = []
+rotate xs pos
+  | pos < 0   = rotateHelper xs (len + pos)
+  | otherwise = rotateHelper xs pos
+  where len = length xs
+        rotateHelper xs pos = drop pos xs ++ take pos xs
+
+-- Problem 20
+removeAt :: Int -> [a] -> [a]
+
 
